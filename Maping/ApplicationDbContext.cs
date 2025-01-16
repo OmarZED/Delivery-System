98099d683746c4381132634c441715166bae65ce
Omar Zedan <omarzedan1002@gmail.com>
2025-01-16 13:58:27 +0700

    refactor(dish): add logging and exception handling to repository
    Adds logging to all dish repository methods for debugging purposes.
    Adds a try catch for all methods to avoid the program crashing when
    there is an issue with the database.

---
+++ Maping/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Maping
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+   : base(options) { }
+        public DbSet<Dish> Dishes { get; set; }
+        //public DbSet<Basket> Baskets { get; set; }
+        //public DbSet<BasketItem> BasketItems { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+        //public DbSet<OrderItem> OrderItems { get; set; }
+
+        //public DbSet<Rating> Ratings { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Dish>()
+     .Property(d => d.Category)
+     .HasConversion<string>();
+
+        }
+    }
+}
