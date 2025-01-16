b77ee9b9c95a5aff51e0c28aafe85c08f72b2ba6
Omar Zedan <omarzedan1002@gmail.com>
2025-01-16 14:20:23 +0700

    feat(user): add user repository interface
    Adds user repository interface which defines methods for
    user authentication and data access.

---
+++ Interface/IUserRepositry.cs
@@ -0,0 +1,18 @@
+﻿using WebApplication3.Models;
+
+namespace WebApplication3.Interface
+{
+    public interface IUserRepositry
+    {
+        Task<bool> RegisterUserAsync(User user, string password);
+
+        Task<bool> AuthenticateAsync(string email, string password);
+        Task LogoutUserAsync();
+        Task<string> GenerateJwtToken(User user);
+        Task<User?> GetUserByEmailAsync(string email);
+
+        Task<User> GetUserByIdAsync(string userId);
+
+        Task<bool> UpdateUserAsync(User user);
+    }
+}
