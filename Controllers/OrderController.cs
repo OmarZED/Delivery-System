d71b7c3f23d432e682e3dbeb245f93a70797e171
Omar Zedan <omarzedan1002@gmail.com>
2025-01-16 15:49:56 +0700

    feat(order): implement order controller with create and get user orders methods
    Implements the order controller with methods for creating an order
    and getting all the orders for an authenticated user.
    This endpoint is authorized and has input validation.
    Adds logging to all methods and try catch for exception handling.

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using WebApplication3.Dtos.OrderDto;
+using WebApplication3.Interface;
+
+namespace WebApplication3.Controllers
+{
+    [ApiController]
+    [Route("api/order")]
+    [Authorize] // All endpoints require authentication
+    public class OrderController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<OrderController> _logger;
+
+
+        public OrderController(IBasketRepository basketRepository, IOrderRepository orderRepository, IMapper mapper, ILogger<OrderController> logger)
+        {
+            _basketRepository = basketRepository;
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+
+        /// Creates a new order for the authenticated user.
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> CreateOrder([FromBody] CreateOrderDTO createOrderDto)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                _logger.LogWarning("Unauthenticated user attempt to create order.");
+                return BadRequest("User is not authenticated.");
+            }
+
+            try
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                if (string.IsNullOrEmpty(userId))
+                {
+                    _logger.LogWarning("User ID not found in the token when creating order.");
+                    return BadRequest("User ID not found in the token.");
+                }
+                await _orderRepository.CreateOrderAsync(createOrderDto, userId);
+                return Ok(new { message = "Order created successfully" });
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error creating order by user with ID {User.FindFirstValue(ClaimTypes.NameIdentifier)}.");
+                return BadRequest($"Error: {ex.Message}");
+            }
+
+        }
+
+        /// Retrieves all orders placed by the authenticated user
+        [HttpGet("user")]
+        [ProducesResponseType(200, Type = typeof(List<OrderInfoDTO>))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> GetUserOrders()
+        {
+            try
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                if (string.IsNullOrEmpty(userId))
+                {
+                    _logger.LogWarning("Invalid user ID when getting orders.");
+                    return BadRequest("Invalid user ID.");
+                }
+                var orders = await _orderRepository.GetUserOrdersAsync(userId);
+                return Ok(orders);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error getting user orders.");
+                return StatusCode(500, "Internal server error");
+            }
+
+        }
+    }
+}
