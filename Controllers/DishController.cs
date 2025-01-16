202d38c5f3a37a1162f76302cdc5d885ae9d95d5
Omar Zedan <omarzedan1002@gmail.com>
2025-01-16 14:02:45 +0700

    feat(dish): implement dish controller with logging and error handling
    Implement dish api endpoints to fetch dishes with filtering,
    pagination, and rating. Adds logging to all methods for debugging.
    Implements a try catch for all methods to avoid the program crashing
    when there is an issue. The controller handles different response codes
    and exceptions.

---
+++ Controllers/DishController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class DishController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
