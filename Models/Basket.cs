82904638817f5b28cebba628beb3b1b048de3936
Omar Zedan <omarzedan1002@gmail.com>
2025-01-16 14:58:01 +0700

    feat(basket): add basket and basket item entities
    Adds the basket and basket item entity classes with
    their properties and relationships.

---
+++ Models/Basket.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication3.Models
+{
+    public class Basket
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<BasketItem> BasketItems { get; set; } = new List<BasketItem>();
+    }
+}
