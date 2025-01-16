3e33224a93901ccb95604c78cdcc476398f999f3
Omar Zedan <omarzedan1002@gmail.com>
2025-01-16 14:11:35 +0700

    feat(user): add user entity with properties
    Adds a user entity that inherits from IdentityUser and includes
    user specific properties, basket, and orders.

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,17 @@
-﻿namespace WebApplication3.Models
+﻿using Microsoft.AspNetCore.Identity;
+using WebApplication3.Models.Enum;
+
+namespace WebApplication3.Models
 {
-    public class User
+    public class User : IdentityUser
     {
+        public string Name { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string Address { get; set; }
+        public Gender Gender { get; set; }
+
+        public Basket Basket { get; set; }
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+
     }
 }
