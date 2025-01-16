d836fe955321a402881369686905861e2a7853d8
Omar Zedan <omarzedan1002@gmail.com>
2025-01-16 14:31:38 +0700

    feat(user): add register dto for user registration
    Adds a DTO for user registration containing the required properties
    for user registration with data validation.

---
+++ Dtos/UserDto/RegisterDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplication3.Models.Enum;
+
+namespace WebApplication3.Dtos.UserDto
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        [Required]
+        public DateOnly BirthDate { get; set; }
+
+        public string Address { get; set; }
+
+        public Gender Gender { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
